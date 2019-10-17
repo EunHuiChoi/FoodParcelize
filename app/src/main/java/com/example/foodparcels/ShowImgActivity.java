bd1d7d715258ee6582da83b51df23700bfeeccee
EunHuiChoi <eun95828@likelion.org>
2019-10-18 00:46:06 +1100

    add string value

--- app/src/main/java/com/example/foodparcels/ShowImgActivity.java
+++ app/src/main/java/com/example/foodparcels/ShowImgActivity.java
@@ -1,8 +1,10 @@
 package com.example.foodparcels;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,8 @@ public class ShowImgActivity extends AppCompatActivity {
         setContentView(R.layout.activity_showimg);
 
         ImageView image = findViewById(R.id.foodImgView);
+        TextView text = findViewById(R.id.foodTextView);
+        Resources res = getResources();
 
         Intent intent = getIntent();
 
@@ -21,15 +25,19 @@ public class ShowImgActivity extends AppCompatActivity {
         switch (number) {
             case 0:
                 image.setImageResource(R.drawable.pancakes);
+                text.setText(res.getString(R.string.pancakes_intro));
                 break;
             case 1:
                 image.setImageResource(R.drawable.burger);
+                text.setText(res.getString(R.string.burger_intro));
                 break;
             case 2:
                 image.setImageResource(R.drawable.salad);
+                text.setText(res.getString(R.string.salad_intro));
                 break;
             case 3:
                 image.setImageResource(R.drawable.sandwich);
+                text.setText(res.getString(R.string.sandwich_intro));
                 break;
         }
     }
